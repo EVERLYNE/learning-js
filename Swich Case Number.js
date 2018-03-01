7ea8cb914bffe85cf5a24fabf4f6b8944916b6ec
Evelyne Akoth <evelyneoakoth@gmail.com>
2018-03-01 12:01:29 +0300

    sixth commit

---
+++ Swich Case Number.js
@@ -0,0 +1,31 @@
+var DayOfTheWeek = 
+
+switch(DayOfTheWeek){
+	case "Monday":
+	console.log("toaday is monday")
+	break;
+	
+	case "Tuesday":
+	console.log("toay is  tuesday")
+    case "Wednesday":
+	console.log("toay is  wednesday")
+	break;
+	case "Thursday":
+	console.log("toay is  thursday")
+	break;
+
+	case "Friday":
+	console.log("toay is  friday")
+	break;
+
+	
+
+	case "Saturday":
+	console.log("toay is  saturday")
+	break;
+	case "Sunday":
+	console.log("toay is  sunday")
+	break;
+	default:
+	console.log("error")
+	break;
---
+++ switch case.js
@@ -0,0 +1,13 @@
+var isSunday = false
+
+switch(isSunday){
+	case true:
+	console.log("Toaday is Sunday")
+	break;
+	case false:
+	console.log("Toay is not Sunday")
+	break;
+	default:
+	console.log("Error")
+	break;
+}
