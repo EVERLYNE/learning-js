33aaf0337f95deb438c7537be4942e3c577fa863
Evelyne Akoth <evelyneoakoth@gmail.com>
2018-03-09 15:20:28 +0300

    sixth commit

---
+++ Functions.js
@@ -0,0 +1,28 @@
+//This is a void function
+functions add(){
+	var numberOne =4;
+	var numberTwo =5;
+	var sum = numberOne + numberTwo;
+	console.log(sum);
+}
+// call the function
+fuction multiply( ){
+     var numberOne=4
+     var numberTwo =5
+     var numberThree= 6
+
+     var answer = numberOne + numberTwo + numberThree;
+     return (ansewr)
+}
+add()
+var result = multiply()
+console.log(result)
+
+fuction divide(){
+	var numberOne=10;
+	var numberTwo=2;
+	var answer = numberOne/numberTwo;
+	return answer;
+}
+var result = divide();
+console.log(result);
