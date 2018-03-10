8387b4eb92277c6abcc399464ce0d38ca2449a8a
Evelyne Akoth <evelyneoakoth@gmail.com>
2018-03-10 11:59:27 +0300

    seventh commit

---
+++ Arrays.js
@@ -0,0 +1,7 @@
+var student = ["Jane","Mary","Laureen","Ivy","Fatma","Judith","Irene"]
+var shoeSize = [31,40,30,41,60.45,70]
+// Get the length of an array
+// console.log(student.length)
+for (i=0;i<student.length;i+=1){
+	console.log(student[i]);
+}
---
+++ FunctionParameter.js
@@ -0,0 +1,16 @@
+function AreaofTriangle(height, base){
+	var area = 0.5 * height * base
+	return area
+
+
+}  
+var result = AreaofTriangle(20,22)
+var result2 = AreaofTriangle(24,26)
+var result3= AreaofTriangle(22,26)
+
+
+
+function print( answer){
+   console.log("AreaofTriangle: "+ answer)
+
+}
