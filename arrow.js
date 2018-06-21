ed16784055a1c9fb2b05259e0cc112dfc87b8fa2
EVERLYNE <35220417+EVERLYNE@users.noreply.github.com>
2018-06-21 16:01:51 +0300

    added a file

---
+++ arrow.js
@@ -0,0 +1,11 @@
+var multiply=(x,y,z)=>x*y*z;
+
+console.log(multiply(8,9,11))
+
+
+var addition=(e,f,g)=>e+f+g;
+console.log(addition(10,20,30))
+
+
+var subtraction=(h,i,j)=>h-i-j;
+console.log(subtraction(100,40,10))
